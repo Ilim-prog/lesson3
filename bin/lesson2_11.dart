23861f3df80fb78d5c6de7c3206e683307da1e81
Ilim-prog <ilimbek.baturbek.01@gmail.com>
2022-07-01 19:46:11 +0600

    first commit

---
+++ bin/lesson2_11.dart
@@ -0,0 +1,82 @@
+import 'package:lesson2_11/lesson2_11.dart' as lesson2_11;
+
+void main() {
+  var a = '1';
+  if (a == '1' && a.runtimeType == String) {
+    print('verno');
+  } else {
+    print('neverno');
+  }
+
+  // dynamic a = 'test';
+  // if (a == -3) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // task7
+  // int a = 0;
+  // if (a != 0) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  //task 6
+  // int a = 0;
+  // if (a <= -3) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // int a = 0;
+  // if (a >= 0) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // int a = 0;
+  // if (a < -3) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // int a = 0;
+  // if (a > 1) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // int a = 0;
+  // if (a == -3) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+
+  // bool isRain = true;
+  // int qwe = 10;
+  // print(qwe.runtimeType);
+  // if (isRain) {
+  //   print('возьмите зонт');
+  // } else {
+  //   print('зонт не нужен');
+  // }
+
+  // isRain == true ? print('возьмите зонт') : print('зонт не нужен');
+}
+
+
+  // int a = 105;
+  // if (a != 10) {
+  //   print('verno');
+  // } else {
+  //   print('neverno');
+  // }
+  // int a = 0;
+  // if (a == 10)
